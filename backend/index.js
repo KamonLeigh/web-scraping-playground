96009d605682f50a167019f55d5f9c12e2fdce4e
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-06-14 17:40:31 +0100

    add nodemon script to start backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const chalk = require("chalk");
+const cors = require("cors");
 
 const app = express();
-
+app.use(cors());
 
 
 app.listen(3000, () => console.log(chalk.green("backend end server has started")));
