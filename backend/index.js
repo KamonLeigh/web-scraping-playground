23e4fb15bee57b43a4bfc9ab120c0ffd2486a8eb
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-06-14 18:24:36 +0100

    Add basic get route

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,11 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+app.get("/", (req, res) =>{
+
+    res.json({"hellow": "world"});
+});
+
 
 app.listen(3000, () => console.log(chalk.green("backend end server has started")));
 
