d20f7b4c7aa7b24805ce2be2c030a0dd1b384577
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-06-18 21:58:05 +0100

    change style of main header

--- frontend/components/styles/Title.js
+++ frontend/components/styles/Title.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 const Title = styled.h1`
-  font-size: 2.2rem;
+  font-size: 2.4rem;
   grid-column: 2 / -2;
   margin: 0;
   margin-block-end: 1em;
@@ -9,9 +9,20 @@ const Title = styled.h1`
   text-align: center;
   letter-spacing: 1.3px;
   color: #4c4e4d;
+  place-self: center;
+  font-weight: bold;
 
   :hover {
     color: #2f3033;
+  }
+  ::after {
+    content: "";
+    width: 100%;
+    height: 20px;
+    background-color: #ffff0091;
+    display: block;
+    margin-top: -35px;
+    margin-left: 10px;
   }
 `;
 
